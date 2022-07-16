5e794319d3881bfbf0b728c26015b93d5ebf986c
Boatyy <bcou94@gmail.com>
2022-07-16 14:23:33 -0700

    added buttons for languages. create new card function, display flashcard question, display flashcard answer. toDo: make array for each language. have each button pull from its language array."

--- script.js
+++ script.js
@@ -2,18 +2,45 @@ const flashCards = []
 
 const flashCard = document.querySelector('.flashcard')
 const flashLanguage = document.createElement('div')
+flashLanguage.classList.add('language')
 const flashQuestion = document.createElement('div')
+flashQuestion.classList.add('question')
 const flashAnswer = document.createElement('div')
+flashAnswer.classList.add('answer')
 const flashReference = document.createElement('div')
-document.addEventListener( "click", someListener );
+flashReference.classList.add('reference')
+const showAnswer = document.createElement('button')
+showAnswer.classList.add('answerButton')
+showAnswer.textContent= 'Show Answer'
+showAnswer.id = ('answerButton')
+document.addEventListener( 'click', someListener );
+let flashlength = flashCards.length
+showAnswer.addEventListener('click', displayAnswer)
 
-
+function displayAnswer(){
+    flashAnswer.style.visibility = 'visible'
+    flashReference.style.visibility = 'visible'
+    flashCard.removeChild(showAnswer)
+}
 
 function someListener(event){
     var element = event.target;
+    var random = Math.floor((Math.random()*flashCards.length));
+
     if(element.id == 'html'){
-        console.log(element)
-        console.log(flashCards)
+        console.log(flashCards[random])
+        let i = random
+        flashLanguage.textContent = flashCards[i].language
+        flashQuestion.textContent = flashCards[i].question
+        flashAnswer.textContent = flashCards[i].answer
+        flashAnswer.style.visibility = 'hidden'
+        flashReference.textContent = flashCards[i].reference 
+        flashReference.style.visibility = 'hidden'
+        flashCard.appendChild(flashLanguage)
+        flashCard.appendChild(flashQuestion)
+        flashCard.appendChild(showAnswer)
+        flashCard.appendChild(flashAnswer)
+        flashCard.appendChild(flashReference)
     }else if(element.id == 'css'){
         console.log(element)
     }else if (element.id == 'javascript'){
@@ -22,7 +49,7 @@ function someListener(event){
 }
 
 function getHtmlCard(){
-    
+
 }
 
 //
@@ -34,14 +61,26 @@ function Card(language, question, answer, reference){
     flashCards.push(this)
 }
 
-const htmlcard1 = new Card( 'HTML', 
-                            'How do you link a CSS stylesheet to html?',
-                            '<link rel="stylesheet" href="style.css">',
-                            'https://www.w3schools.com/tags/tag_link.asp'
+new Card(   '1HTML', 
+            'How do you link a CSS stylesheet to html?',
+            '<link rel="stylesheet" href="style.css">',
+            'https://www.w3schools.com/tags/tag_link.asp'
 )
-const htmlcard2 = new Card( 'HTML', 
-                            'How do you link a CSS stylesheet to html?',
-                            '<link rel="stylesheet" href="style.css">',
-                            'https://www.w3schools.com/tags/tag_link.asp'
+
+new Card(   '2HTML', 
+            'How do you link a CSS stylesheet to html?',
+            '<link rel="stylesheet" href="style.css">',
+            'https://www.w3schools.com/tags/tag_link.asp'
 )
+new Card(   '3HTML', 
+            'How do you link a CSS stylesheet to html?',
+            '<link rel="stylesheet" href="style.css">',
+            'https://www.w3schools.com/tags/tag_link.asp'
+)
+new Card(   '4HTML', 
+            'How do you link a CSS stylesheet to html?',
+            '<link rel="stylesheet" href="style.css">',
+            'https://www.w3schools.com/tags/tag_link.asp'
+)
+
 
