96cd347bcac10df6ce3c3258d5ff0e5ff44bbc3d
Boatyy <bcou94@gmail.com>
2022-07-17 13:19:32 -0700

    array created for css and js. functions created to push obj to each array. populating cards

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
-const flashCards = []
+//flash card arrays
+const htmlCards = []
+const cssCards = []
+const jsCards = []
 
+//dom targets
 const flashCard = document.querySelector('.flashcard')
 const flashLanguage = document.createElement('div')
 flashLanguage.classList.add('language')
@@ -14,9 +18,9 @@ showAnswer.classList.add('answerButton')
 showAnswer.textContent= 'Show Answer'
 showAnswer.id = ('answerButton')
 document.addEventListener( 'click', someListener );
-let flashlength = flashCards.length
 showAnswer.addEventListener('click', displayAnswer)
 
+//function to show answer and ref documents 
 function displayAnswer(){
     flashAnswer.style.visibility = 'visible'
     flashReference.style.visibility = 'visible'
@@ -25,11 +29,13 @@ function displayAnswer(){
 
 function someListener(event){
     var element = event.target;
-    var random = Math.floor((Math.random()*flashCards.length));
+    var randomhtml = Math.floor((Math.random()*htmlCards.length));
+    var randomcss = Math.floor((Math.random()*htmlCards.length));
+    var randomjs = Math.floor((Math.random()*htmlCards.length));
+
 
     if(element.id == 'html'){
-        console.log(flashCards[random])
-        let i = random
+        let i = randomhtml
         flashLanguage.textContent = flashCards[i].language
         flashQuestion.textContent = flashCards[i].question
         flashAnswer.textContent = flashCards[i].answer
@@ -42,9 +48,31 @@ function someListener(event){
         flashCard.appendChild(flashAnswer)
         flashCard.appendChild(flashReference)
     }else if(element.id == 'css'){
-        console.log(element)
+        let i = randomcss
+        flashLanguage.textContent = flashCards[i].language
+        flashQuestion.textContent = flashCards[i].question
+        flashAnswer.textContent = flashCards[i].answer
+        flashAnswer.style.visibility = 'hidden'
+        flashReference.textContent = flashCards[i].reference 
+        flashReference.style.visibility = 'hidden'
+        flashCard.appendChild(flashLanguage)
+        flashCard.appendChild(flashQuestion)
+        flashCard.appendChild(showAnswer)
+        flashCard.appendChild(flashAnswer)
+        flashCard.appendChild(flashReference)
     }else if (element.id == 'javascript'){
-        console.log(element)
+        let i = randomjavascript
+        flashLanguage.textContent = flashCards[i].language
+        flashQuestion.textContent = flashCards[i].question
+        flashAnswer.textContent = flashCards[i].answer
+        flashAnswer.style.visibility = 'hidden'
+        flashReference.textContent = flashCards[i].reference 
+        flashReference.style.visibility = 'hidden'
+        flashCard.appendChild(flashLanguage)
+        flashCard.appendChild(flashQuestion)
+        flashCard.appendChild(showAnswer)
+        flashCard.appendChild(flashAnswer)
+        flashCard.appendChild(flashReference)
     }
 }
 
@@ -52,35 +80,84 @@ function getHtmlCard(){
 
 }
 
-//
-function Card(language, question, answer, reference){
+//push to HTML array (htmlCards)
+function htmlCard(language, question, answer, reference){
     this.language = language;
     this.question = question;
     this.answer = answer;
     this.reference = reference;
-    flashCards.push(this)
+    htmlCards.push(this)
 }
 
-new Card(   '1HTML', 
-            'How do you link a CSS stylesheet to html?',
-            '<link rel="stylesheet" href="style.css">',
-            'https://www.w3schools.com/tags/tag_link.asp'
-)
+//push to CSS array (cssCards)
+function cssCard(language, question, answer, reference){
+    this.language = language;
+    this.question = question;
+    this.answer = answer;
+    this.reference = reference;
+    cssCards.push(this)
+}
 
-new Card(   '2HTML', 
-            'How do you link a CSS stylesheet to html?',
-            '<link rel="stylesheet" href="style.css">',
-            'https://www.w3schools.com/tags/tag_link.asp'
-)
-new Card(   '3HTML', 
-            'How do you link a CSS stylesheet to html?',
-            '<link rel="stylesheet" href="style.css">',
-            'https://www.w3schools.com/tags/tag_link.asp'
-)
-new Card(   '4HTML', 
-            'How do you link a CSS stylesheet to html?',
-            '<link rel="stylesheet" href="style.css">',
-            'https://www.w3schools.com/tags/tag_link.asp'
-)
+//push to javascript array (jsCards)
+function jsCard(language, question, answer, reference){
+    this.language = language;
+    this.question = question;
+    this.answer = answer;
+    this.reference = reference;
+    jsCards.push(this)
+}
 
 
+//      new htmlCard('','','','')
+//      new cssCard('','','','')
+//      new jsCard('','','','')
+
+
+//ADD NEW HTML CARDS HERE HTMLCARDADD
+//      new htmlCard('','','','')
+new htmlCard(   'HTML', 
+                'How do you link a CSS stylesheet to html?',
+                '<link rel="stylesheet" href="style.css">',
+                'https://www.w3schools.com/tags/tag_link.asp'
+)
+new htmlCard(   'HTML', 
+                'What is the correct HTML element for the largest heading?',
+                '<h1>',
+                'https://www.w3schools.com/tags/tag_hn.asp'
+)
+
+new htmlCard(   'HTML', 
+                'How do you define important text?',
+                '<strong>',
+                'https://www.w3schools.com/tags/tag_strong.asp'
+)
+new htmlCard(   'HTML',
+                'How do you link an external JavaScript file?',
+                '<script src="myscripts.js"></script>',
+                'https://www.w3schools.com/tags/att_script_src.asp')
+new htmlCard('','','','')
+new htmlCard('','','','')
+new htmlCard('','','','')
+new htmlCard('','','','')
+
+//ADD NEW CSS CARDS HERE CSSCARDADD
+//      new cssCard('','','','')
+new cssCard('','','','')
+new cssCard('','','','')
+new cssCard('','','','')
+new cssCard('','','','')
+new cssCard('','','','')
+new cssCard('','','','')
+new cssCard('','','','')
+new cssCard('','','','')
+
+//ADD NEW JAVASCRIPT CARDS HERE JSCARDADD
+//      new jsCard('','','','')
+new jsCard('','','','')
+new jsCard('','','','')
+new jsCard('','','','')
+new jsCard('','','','')
+new jsCard('','','','')
+new jsCard('','','','')
+new jsCard('','','','')
+new jsCard('','','','')
